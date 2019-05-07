a449a17a55bed3a7d02b08d08790096c7d4d1b33
Robert Tibbs <tibbsr09@gmail.com>
2019-05-06 21:41:45 -0400

    added the potential friends you can match up with, added photo links, and procided an array of scores to matcht with.

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+    {
+        name: "Dennis Reynolds",
+        photo: "http://bh-s2.azureedge.net/bh-uploads/2016/09/1-Dennis-1050x646.jpg",
+        score: [5, 4, 3, 2, 2, 1, 4, 5, 3, 2]
+
+    },
+    {
+        name: "Frank Reynolds",
+        photo: "https://images-cdn.9gag.com/photo/ad93b02_700b.jpg",
+        score: [3, 4, 3, 2, 1, 1, 5, 5, 3, 4]
+     
+    },
+    {
+        name: "Dee Reynolds",
+        photo: "https://uproxx.files.wordpress.com/2015/09/dee-always-sunny.jpg?quality=100&w=650",
+        score: [5, 5, 2, 1, 2, 3, 4, 2, 3, 5]
+    },
+    {
+        name: "Ronald Mac McDonald",
+        photo: "https://img.huffingtonpost.com/asset/5bb4dd8d24000031005633a6.jpeg?ops=scalefit_720_noupscale",
+        score: [2, 2, 1, 1, 3, 4, 5, 5, 2, 3]
+    },
+    {
+        name:"Charlie Kelly",
+        photo: "https://i0.wp.com/www.pinkink.media/wp-content/uploads/2015/07/dayman-charlie-kelly.jpg?resize=512%2C384&ssl=1",
+        score: [3, 4, 5, 3, 2, 4, 1, 5, 2, 1]
+    },
+    {
+        name: "Rickety Cricket",
+        photo: "https://assets.fxnetworks.com/cms/prod/2018/08/07/web_cast_davidhornsby_sunny_570x698.jpg?resize=570:*",
+        score: [4, 4, 2, 3, 2, 1, 5, 2, 5, 3]
+    }
+
+];
+
+module.exports = friends;
